065e44b20cf8abecb431bcb14309c219d9e326f6
linyang@offcn.com <lin2798003@sina.com>
2018-07-03 21:02:34 +0800

    优化start.php

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,13 +4,16 @@ nginxDefaultConf="/etc/nginx/conf.d/default.conf"
 
 #nginx php conf select
 if [ "$NGINX_PHP_CONF" == "tp" ];then
+    echo "nginx-fastcgi-conf: thinkphp"
     cat /nginx-php-conf/tp.conf > ${nginxDefaultConf}
 elif [ "$NGINX_PHP_CONF" == "orc" ];then
+    echo "nginx-fastcgi-conf: orc"
     cat /nginx-php-conf/orc.conf > ${nginxDefaultConf}
 elif [ "$NGINX_PHP_CONF" == "laravel" ];then
+    echo "nginx-fastcgi-conf: laravel"
     cat /nginx-php-conf/laravel.conf > ${nginxDefaultConf}
 else
-    echo "yaf"
+    echo "nginx-fastcgi-conf: yaf"
 fi
 
 
@@ -22,21 +25,6 @@ fi
 # Increase the nginx default.conf
 if [ ! -z "$APP_PATH_404" ]; then
  sed -i "s#root /var/www/errors;#root ${APP_PATH_404};#g" ${nginxDefaultConf}
-fi
-
-# Increase the memory_limit
-if [ ! -z "$PHP_MEM_LIMIT" ]; then
- sed -i "s#memory_limit = 128M#memory_limit = ${PHP_MEM_LIMIT}M#g" /usr/local/etc/php/conf.d/docker-vars.ini
-fi
-
-# Increase the post_max_size
-if [ ! -z "$PHP_POST_MAX_SIZE" ]; then
- sed -i "s#post_max_size = 100M#post_max_size = ${PHP_POST_MAX_SIZE}M#g" /usr/local/etc/php/conf.d/docker-vars.ini
-fi
-
-# Increase the upload_max_filesize
-if [ ! -z "$PHP_UPLOAD_MAX_FILESIZE" ]; then
- sed -i "s#upload_max_filesize = 100M#upload_max_filesize= ${PHP_UPLOAD_MAX_FILESIZE}M#g" /usr/local/etc/php/conf.d/docker-vars.ini
 fi
 
 # 环境变量配置替换
